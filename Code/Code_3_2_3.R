60520ee66e39273792878ebcae32eb11dd21b199
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-04-12 18:21:40 +0900

    Update Code_3_2_3.R

--- Code/Code_3_2_3.R
+++ Code/Code_3_2_3.R
@@ -1,11 +1,10 @@
 
 # Gibbs sampler for LDA ---------------------------------------------------
 
-
 # 利用パッケージ
 library(tidyverse)
 
-## 動作検証用
+## 簡易文書データ
 # 文書数
 M <- 10
 # 語彙数
@@ -16,14 +15,13 @@ n_dv <- matrix(sample(1:3, M * V, replace = TRUE), M, V)
 
 # パラメータの設定 -----------------------------------------------------------------
 
+# サンプリング回数を指定
+S <- 100
 
-# サンプリング回数(試行回数)
-S <- 1000
-
-# トピック数
+# トピック数を指定
 K <- 5
 
-# 事前分布のパラメータ:(1以上の値)
+# 事前分布のパラメータを指定:(1以上の値)
 alpha_k <- rep(2, K)
 beta_v  <- rep(2, V)
 
@@ -45,27 +43,21 @@ phi_kv <- phi_kv / apply(phi_kv, 1, sum)
 z_di <- array(0, dim = c(M, V, max(n_dv)))
 
 # 各文書において各トピックが割り当てられた単語数
-n_dk <- new_n_dk <- matrix(0, nrow = M, ncol = K)
+n_dk <- matrix(0, nrow = M, ncol = K)
 
 # 全文書において各トピックが割り当てられた単語数
-n_kv <- new_n_kv <- matrix(0, nrow = K, ncol = V)
+n_kv <- matrix(0, nrow = K, ncol = V)
 
 
 # ギブスサンプリング ----------------------------------------------------------------------
 
-# 推移の確認用データフレームを作成
-trace_theta <- cbind(
-  as.data.frame(theta_dk), 
-  doc = as.factor(1:M),  # 文書番号
-  S = 0 # 試行回数
-)
-trace_phi <- cbind(
-  as.data.frame(phi_kv), 
-  topic = as.factor(1:K),  # トピック番号
-  S = 0 # サンプリング回数
-)
+# 推移の確認用
+trace_theta <- array(0, dim = c(M, K, S + 1))
+trace_phi   <- array(0, dim = c(K, V, S + 1))
+# 初期値を代入
+trace_theta[, , 1] <- theta_dk
+trace_phi[, , 1]   <- phi_kv
 
-# 推定
 for(s in 1:S) { ## (イタレーション)
   
   for(d in 1:M) { ## (各文書)
@@ -90,7 +82,7 @@ for(s in 1:S) { ## (イタレーション)
     new_alpha_k <- n_dk[d, ] + alpha_k
     
     # theta_{d,k}をサンプリング
-    theta_dk[d, ] <- MCMCpack::rdirichlet(1, alpha = new_alpha_k - 1) %>% 
+    theta_dk[d, ] <- MCMCpack::rdirichlet(n = 1, alpha = new_alpha_k - 1) %>% 
       as.vector()
     
   } ## (/各文書)
@@ -101,9 +93,8 @@ for(s in 1:S) { ## (イタレーション)
     new_beta_v <- n_kv[k, ] + beta_v
     
     # phi_{k,v}をサンプリング
-    phi_kv[k, ] <- MCMCpack::rdirichlet(1, alpha = new_beta_v - 1) %>% 
+    phi_kv[k, ] <- MCMCpack::rdirichlet(n = 1, alpha = new_beta_v - 1) %>% 
       as.vector()
-    
     
     # カウントを更新
     n_dk[, k] <- apply(z_di == k, 1, sum)
@@ -112,25 +103,13 @@ for(s in 1:S) { ## (イタレーション)
   } ## (/各トピック)
   
   # パラメータを正規化
-  phi_kv   <- phi_kv / apply(phi_kv, 1, sum)
   theta_dk <- theta_dk / apply(theta_dk, 1, sum)
+  phi_kv   <- phi_kv / apply(phi_kv, 1, sum)
   
+  # 推移の確認用
+  trace_theta[, , s + 1] <- theta_dk
+  trace_phi[, , s + 1]   <- phi_kv
   
-  # 推移の確認用データフレームを作成
-  tmp_trace_theta <- cbind(
-    as.data.frame(theta_dk), 
-    doc = as.factor(1:M),  # 文書番号
-    S = s # 試行回数
-  )
-  tmp_trace_phi <- cbind(
-    as.data.frame(phi_kv), 
-    topic = as.factor(1:K),  # トピック番号
-    S = s # 試行回数
-  )
-  
-  # データフレームを結合
-  trace_theta <- rbind(trace_theta, tmp_trace_theta)
-  trace_phi <- rbind(trace_phi, tmp_trace_phi)
 }
 
 # 処理の検証
@@ -141,7 +120,6 @@ apply(theta_dk, 1, sum)
 
 
 # 推定結果の確認 ----------------------------------------------------------------------
-
 
 ## トピック分布
 # 作図用のデータフレームを作成
@@ -186,7 +164,7 @@ phi_LongDF <- pivot_longer(
 )
 
 # 作図
-ggplot(phi_LongDF, aes(x = word, y = prob, fill = word)) + 
+ggplot(phi_LongDF, aes(x = word, y = prob, fill = word, color = word)) + 
   geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
   facet_wrap( ~ topic, labeller = label_both) + # グラフの分割
   scale_x_discrete(breaks = seq(1, V, by = 10)) + # x軸目盛
@@ -200,18 +178,46 @@ ggplot(phi_LongDF, aes(x = word, y = prob, fill = word)) +
 # 利用パッケージ
 library(gganimate)
 
+# データフレームに変換
+trace_theta_WideDF <- data.frame()
+trace_phi_WideDF <- data.frame()
+for(s in 1:(S + 1)) {
+  tmp_trace_theta <- cbind(
+    as.data.frame(trace_theta[, , s]), 
+    doc = as.factor(1:M), # 文書番号
+    S = s - 1 # 試行回数
+  )
+  tmp_trace_phi <- cbind(
+    as.data.frame(trace_phi[, , s]), 
+    topic = as.factor(1:K), # トピック番号
+    S = s - 1 # 試行回数
+  )
+  
+  # データフレームを結合
+  trace_theta_WideDF <- rbind(trace_theta_WideDF, tmp_trace_theta)
+  trace_phi_WideDF <- rbind(trace_phi_WideDF, tmp_trace_phi)
+}
 
-## トピック分布
 # データフレームをlong型に変換
 trace_theta_LongDF <- pivot_longer(
-  trace_theta, 
+  trace_theta_WideDF, 
   cols = -c(doc, S),   # 変換せずにそのまま残す現列名
   names_to = "topic",  # 現列名を格納する新しい列の名前
   names_prefix = "V",  # 現列名から取り除く文字列
   names_ptypes = list(topic = factor()),  # 現列名を要素とする際の型
   values_to = "prob"   # 現要素を格納する新しい列の名前
 )
+trace_phi_LongDF <- pivot_longer(
+  trace_phi_WideDF, 
+  cols = -c(topic, S), # 変換せずにそのまま残す現列名
+  names_to = "word",   # 現列名を格納する新しい列の名前
+  names_prefix = "V",  # 現列名から取り除く文字列
+  names_ptypes = list(word = factor()),  # 現列名を要素とする際の型
+  values_to = "prob"   # 現要素を格納する新しい列の名前
+)
 
+
+## トピック分布
 # 作図
 graph_theta <- ggplot(trace_theta_LongDF, aes(x = topic, y = prob, fill = topic)) + 
   geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
@@ -225,18 +231,8 @@ animate(graph_theta, nframes = S + 1, fps = 10)
 
 
 ## 単語分布
-# 作図用のデータフレームを作成
-trace_phi_LongDF <- pivot_longer(
-  trace_phi, 
-  cols = -c(topic, S), # 変換せずにそのまま残す現列名
-  names_to = "word",   # 現列名を格納する新しい列の名前
-  names_prefix = "V",  # 現列名から取り除く文字列
-  names_ptypes = list(word = factor()),  # 現列名を要素とする際の型
-  values_to = "prob"   # 現要素を格納する新しい列の名前
-)
-
 # 作図
-graph_phi <- ggplot(trace_phi_LongDF, aes(x = word, y = prob, fill = word)) + 
+graph_phi <- ggplot(trace_phi_LongDF, aes(x = word, y = prob, fill = word, color = word)) + 
   geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
   facet_wrap( ~ topic, labeller = label_both) +      # グラフの分割
   scale_x_discrete(breaks = seq(1, V, by = 10)) +    # x軸目盛
