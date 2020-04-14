361a8fdb78332a3fde8e42ebd205a7b500a25cd3
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-04-14 16:14:10 +0900

    Update Code_3_3_5_2.R

--- Code/Code_3_3_5_2.R
+++ Code/Code_3_3_5_2.R
@@ -16,111 +16,108 @@ n_d <- apply(n_dv, 1, sum)
 
 # パラメータの設定 ----------------------------------------------------------------
 
-# イタレーション数
+# イタレーション数を指定
 OutIter <- 50
 InIter  <- 10
+
+# 絶対誤差の上限を指定
 Abs_Err <- 0.1
 
-# トピック数
+# トピック数を指定
 K <- 5
 
 # 事前分布のパラメータ
 alpha_k <- rep(2, K)
 beta_v  <- rep(2, V)
 
-# 潜在トピック集合の分布
-z_dvk <- array(0, dim = c(M, V, K))
+# 潜在トピック集合の分布の初期値
+z_dv_k <- array(0, dim = c(M, V, K))
 for(d in 1:M) {
   for(v in 1:V) {
     # ランダムに値を生成
     tmp_q_z <- seq(0, 1, by = 0.01) %>% 
                sample(size = K, replace = TRUE)
     # 正規化
-    z_dvk[d, v, ] <- tmp_q_z / sum(tmp_q_z)
+    z_dv_k[d, v, ] <- tmp_q_z / sum(tmp_q_z)
   }
 }
 
 ## カウントの期待値
-tmp_z <- array(0, dim = c(M, V, K))
+tmp_n <- array(0, dim = c(M, V, K))
 for(k in 1:K) {
-  tmp_z[, , k] <- z_dvk[, , k] * n_dv
+  tmp_n[, , k] <- z_dv_k[, , k] * n_dv
 }
 
-# 文書ごとにおいて各トピックが割り当てられた単語数
-n_dk <- apply(tmp_z, 1, sum) / K %>% 
-        matrix(nrow = M, ncol = K)
+# 文書ごとにおいて各トピックが割り当てられた単語数の期待値
+E_n_dk <- apply(tmp_n, 1, sum) / K %>% 
+  matrix(nrow = M, ncol = K)
 
-# 全文書において各トピックが割り当てられた単語数
-n_kv <- apply(tmp_z, c(3, 2), sum)
+# 全文書において各トピックが割り当てられた単語数の期待値
+E_n_kv <- apply(tmp_n, c(3, 2), sum)
 
 # 処理の検証用
-sum(n_dk) == sum(n_dv)
-sum(n_kv) == sum(n_dv)
+sum(E_n_dk) == sum(n_dv)
+sum(E_n_kv) == sum(n_dv)
 
 
 # 事後分布パラメータの初期値(=事前分布のパラメータ)
-xi_dk <- t(t(n_dk) + alpha_k)
-xi_kv <- t(t(n_kv) + beta_v)
+xi_dk <- t(t(E_n_dk) + alpha_k)
+xi_kv <- t(t(E_n_kv) + beta_v)
 
 
 # 変分ベイズ -------------------------------------------------------------------
 
-# 推移の確認用のデータフレームを作成
-trace_xi_theta <- cbind(
-  as.data.frame(xi_dk), 
-  doc = as.factor(1:M),  # 文書番号
-  Iter = 0 # 試行回数
-)
-trace_xi_phi <- cbind(
-  as.data.frame(xi_kv), 
-  topic = as.factor(1:K),  # トピック番号
-  Iter = 0 # 試行回数
-)
+# 推移の確認用
+trace_xi_theta <- array(0, dim = c(M, K, OutIter + 1))
+trace_xi_phi   <- array(0, dim = c(K, V, OutIter + 1))
+# 初期値を代入
+trace_xi_theta[, , 1] <- xi_dk
+trace_xi_phi[, , 1]   <- xi_kv
 
-for(OI in 1:OutIter) { ## イタレーション
+E_n_dk_old <- E_n_dk[1, ]
+for(OI in 1:OutIter) { ## (イタレーション)
   
   for(d in 1:M) { ## (各文書)
     
-    # 絶対誤差を計算
-    if(OI > 1) { # 初回を飛ばす
-      abs_err <- sum(abs(n_dk.new - n_dk) / n_d) / K
-    } else if(OI < 1) {
-      abs_err <- Abs_Err + 1
-    }
+    # 絶対誤差を初期化
+    abs_err <- Abs_Err + 1
     # インナーループ回数を初期化
     II <- 1
-    while(abs_err >= Abs_Err && II <= InIter) { ## 
+    while(abs_err >= Abs_Err && II <= InIter) { ## (インナーイタレーション)
       
       # インナーループの回数を更新
       II <- II + 1
       
       for(v in 1:V) { ## (各語彙)
         if(n_dv[d, v] > 0) {
-          for(n in 1:n_dv[d, v]) { ## (各単語)
-            
-            # 潜在トピック集合の事後分布を計算:式(3.99)
-            term1 <- digamma(xi_kv[, v]) - digamma(apply(xi_kv, 1, sum))
-            term2 <- digamma(xi_dk[d, ]) - digamma(apply(xi_dk, 2, sum))
-            tmp_z_dvk <- exp(term1) * exp(term2)
-            # 正規化
-            z_dvk[d, v, ] <- tmp_z_dvk / sum(tmp_z_dvk)
-            
-          } ## (/各単語)
+          
+          # 潜在トピック集合の事後分布を計算:式(3.99)
+          term1 <- digamma(xi_kv[, v]) - digamma(apply(xi_kv, 1, sum))
+          term2 <- digamma(xi_dk[d, ]) - digamma(apply(xi_dk, 2, sum))
+          tmp_q_z <- exp(term1) * exp(term2)
+          # 正規化
+          z_dv_k[d, v, ] <- tmp_q_z / sum(tmp_q_z)
+          
         }
       } ## (/各語彙)
       
       # カウントの期待値
       for(k in 1:K) {
-        tmp_z[, , k] <- z_dvk[, , k] * n_dv
+        tmp_n[, , k] <- z_dv_k[, , k] * n_dv
       }
-      n_dk.new <- apply(tmp_z, 1, sum) / K %>% 
-                  matrix(nrow = M, ncol = K)
-      n_kv <- apply(tmp_z, c(3, 2), sum)
+      E_n_dk <- apply(tmp_n, 1, sum) / K %>% 
+                matrix(nrow = M, ncol = K)
+      E_n_kv <- apply(tmp_n, c(3, 2), sum)
 
       # 事後分布のパラメータを計算:式(3.89)
-      xi_dk[d, ] <- n_dk[d, ] + alpha_k
+      xi_dk[d, ] <- E_n_dk[d, ] + alpha_k
       
-    }
+      # 絶対誤差を計算
+      abs_err <- sum(abs(E_n_dk[d, ] - E_n_dk_old) / n_d[d]) / K
+      # 次回の計算用に値を保存
+      E_n_dk_old <- E_n_dk[d, ]
+      
+    } ## (/インナーイタレーション)
     
     print(paste0("OutIter=", OI, ", InIter=", II, ", Abs_Err=", abs_err))
     
@@ -129,27 +126,16 @@ for(OI in 1:OutIter) { ## イタレーション
   for(k in 1:K) { ## (各トピック)
     
     # 事後分布のパラメータ:式(3.95)
-    xi_kv[k, ] <- n_kv[k, ] + beta_v
+    xi_kv[k, ] <- E_n_kv[k, ] + beta_v
     
   } ## (/各トピック)
   
-  # 推移の確認用のデータフレームを作成
-  tmp_trace_xi_theta <- cbind(
-    as.data.frame(xi_dk), 
-    doc = as.factor(1:M),  # 文書番号
-    Iter = OI # 試行回数
-  )
-  tmp_trace_xi_phi <- cbind(
-    as.data.frame(xi_kv), 
-    topic = as.factor(1:K),  # トピック番号
-    Iter = OI # 試行回数
-  )
-  
-  # データフレームを結合
-  trace_xi_theta <- rbind(trace_xi_theta, tmp_trace_xi_theta)
-  trace_xi_phi   <- rbind(trace_xi_phi, tmp_trace_xi_phi)
+  # 推移の確認用
+  trace_xi_theta[, , OI + 1] <- xi_dk
+  trace_xi_phi[, , OI + 1]   <- xi_kv
 }
 
+warnings() 
 
 # 推定結果の確認 ----------------------------------------------------------------------
 
@@ -218,17 +204,45 @@ ggplot(phi_LongDF, aes(x = word, y = prob, fill = word, color = word)) +
 library(gganimate)
 
 
-## トピック分布
+# データフレームに変換
+trace_xi_theta_WideDF <- data.frame()
+trace_xi_phi_WideDF <- data.frame()
+for(I in 1:(Iter + 1)) {
+  # データフレームに変換
+  tmp_trace_xi_theta <- cbind(
+    as.data.frame(trace_xi_theta[, , I]), 
+    doc = as.factor(1:M), # 文書番号
+    Iter = I - 1 # 試行回数
+  )
+  tmp_trace_xi_phi <- cbind(
+    as.data.frame(trace_xi_phi[, , I]), 
+    topic = as.factor(1:K), # トピック番号
+    Iter = I - 1 # 試行回数
+  )
+  # 結合
+  trace_xi_theta_WideDF <- rbind(trace_xi_theta_WideDF, tmp_trace_xi_theta)
+  trace_xi_phi_WideDF <- rbind(trace_xi_phi_WideDF, tmp_trace_xi_phi)
+}
+
 # データフレームをlong型に変換
 trace_xi_theta_LongDF <- pivot_longer(
-  trace_xi_theta, 
+  trace_xi_theta_WideDF, 
   cols = -c(doc, Iter),          # 変換せずにそのまま残す現列名
   names_to = "topic",   # 現列名を格納する新しい列の名前
   names_prefix = "V",  # 現列名から取り除く文字列
   names_ptypes = list(topic = factor()),  # 現列名を要素とする際の型
   values_to = "value"    # 現要素を格納する新しい列の名前
 )
+trace_xi_phi_LongDF <- pivot_longer(
+  trace_xi_phi_WideDF, 
+  cols = -c(topic, Iter),        # 変換せずにそのまま残す現列名
+  names_to = "word",    # 現列名を格納する新しい列の名前
+  names_prefix = "V",  # 現列名から取り除く文字列
+  names_ptypes = list(word = factor()),  # 現列名を要素とする際の型
+  values_to = "value"    # 現要素を格納する新しい列の名前
+)
 
+## トピック分布
 # 作図
 graph_xi_theta <- ggplot(trace_xi_theta_LongDF, aes(x = topic, y = value, fill = topic)) + 
   geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
@@ -243,16 +257,6 @@ animate(graph_xi_theta, nframes = (OutIter + 1), fps = 10)
 
 
 ## 単語分布
-# 作図用のデータフレームを作成
-trace_xi_phi_LongDF <- pivot_longer(
-  trace_xi_phi, 
-  cols = -c(topic, Iter),        # 変換せずにそのまま残す現列名
-  names_to = "word",    # 現列名を格納する新しい列の名前
-  names_prefix = "V",  # 現列名から取り除く文字列
-  names_ptypes = list(word = factor()),  # 現列名を要素とする際の型
-  values_to = "value"    # 現要素を格納する新しい列の名前
-)
-
 # 作図
 graph_xi_phi <- ggplot(trace_xi_phi_LongDF, aes(x = word, y = value, fill = word, color = word)) + 
   geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
